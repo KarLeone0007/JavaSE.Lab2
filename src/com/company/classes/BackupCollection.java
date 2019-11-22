c0136df836f7004db9e2ba5697e70b4231ab5a0a
manlemmi <dr.radov2010@gmail.com>
2019-11-22 15:37:29 +0200

    commit code

--- src/com/company/classes/BackupCollection.java
+++ src/com/company/classes/BackupCollection.java
@@ -6,14 +6,14 @@ import com.google.gson.GsonBuilder;
 import java.io.*;
 
 class BackupCollection {
-    public void writeJSON(Parking parking, String fileName) throws IOException {
+    public void writeJSON(Parking parking, String fileName) throws IOException {    //метод для записи списка в файл JSON
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
         FileWriter writer = new FileWriter(fileName);
         writer.write(gson.toJson(parking));
         writer.close();
     }
-    public Parking readJSON(String fileName) throws FileNotFoundException {
+    public Parking readJSON(String fileName) throws FileNotFoundException { //метод для чтения
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
         BufferedReader bufferedReader = new BufferedReader(
